8b839e7a1b02485e81ab74adcec5db06b11923d0
lavi061997 <lavi061997@gmail.com>
2017-05-14 19:48:45 +0530

    Commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+	public float speed = 5f;
+	public float incSpeed = 10f;
+	public float maxDist = 12f;
+	public bool counter = true;
+
+	public Rigidbody2D rb;
+
+
+	void Start()
+	{
+		rb.velocity = new Vector2(0, speed);
+		// Initially object is facing up (Y-axis)
+		transform.localRotation = Quaternion.Euler(Vector3.up);
+	}
+
+
+	private void FixedUpdate()
+	{
+		
+		if (Input.GetButtonDown("Jump"))
+		{
+			if (counter)
+			{
+				rb.velocity = new Vector2(incSpeed, speed);
+				counter = !counter;
+
+			}
+			else
+			{
+				rb.velocity = new Vector2(-incSpeed, speed);
+				counter = !counter;
+
+			}
+		}
+
+		if (rb.position.x > 6)
+		{
+			Left();
+		}
+
+		if(rb.position.x< -6)
+		{
+			Right();
+		}
+	}
+	public void Left()
+	{
+
+		rb.velocity = new Vector2(0, speed);
+		// Initially object is facing up (Y-axis)
+		transform.localRotation = Quaternion.Euler(Vector3.up);
+		transform.position = new Vector2(5.9f, rb.position.y);
+	}
+
+	public void Right()
+	{
+
+		rb.velocity = new Vector2(0, speed);
+		// Initially object is facing up (Y-axis)
+		transform.localRotation = Quaternion.Euler(Vector3.up);
+		transform.position = new Vector2(-5.9f, rb.position.y);
+
+	}
+
+}
---
+++ Assets/Block.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{	if (col.tag == "Ball")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
